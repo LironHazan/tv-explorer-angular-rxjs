5813849a504141d82300792f6e16fc2029e0ccf6
lironh <lironenk@gmail.com>
2017-10-11 11:29:27 +0300

    feat(first commit, has issue in getting output)

---
+++ src/app/search/search.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import '../../../node_modules/rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class SearchService {
+
+  constructor(@Inject('tvAPI') private tvAPI, private http:HttpClient) {}
+
+  searchTvShow(query) {
+    return this.http.get(`${this.tvAPI}search/shows?q=:${query}/`)
+      .toPromise();
+  }
+
+}
---
+++ src/app/search/search/search.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { SearchService } from '../../search/search.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+
+export class SearchComponent implements OnInit {
+
+  constructor(private searchService: SearchService) {
+  }
+
+  ngOnInit() {
+  }
+
+  @Output() onShowsResult = new EventEmitter();
+
+  @Output() public notify: EventEmitter<string> = new EventEmitter<string>();
+
+  onClick(){
+    this.notify.emit('msg from search component');
+  }
+  values = '';
+
+  searchTvShow(e: any){
+    if(e.keyCode === 13){
+      if(e.target.value){
+        this.values = e.target.value;
+        this.searchService.searchTvShow(e.target.value)
+          .then((data) => {
+            console.log(data);
+            this.onShowsResult.emit(data);
+          });
+      }
+    }
+  }
+
+}
